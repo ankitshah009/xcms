d74f802bb502105836c1e16f328dfda8e488d98c
Ankit Shah <ashah@revivemed.io>
2020-03-10 18:05:36 -0400

    updating

--- R/do_findChromPeaks-functions.R
+++ R/do_findChromPeaks-functions.R
@@ -440,7 +440,7 @@ do_findChromPeaks_centWave <- function(mz, int, scantime, valsPerSpect,
 
 
 
-    save(ll, file="ll.RData")
+    saveRDS(ll, file="ll.rds")
 
     ## Second stage: process the ROIs
     peaklist <- list()
